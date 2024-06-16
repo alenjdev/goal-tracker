433ff0b4ef7aafd8dcd734bbf4ae77002e787d6d
Alen <alen@formant.io>
2024-06-15 19:20:51 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,5 +1,1 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' });
-  });
-}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/goal-tracker/sw.js', { scope: '/goal-tracker/' })})}
